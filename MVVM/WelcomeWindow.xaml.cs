6b5e6c4d2efb481a27a8a4d9c4f67b8750bad600
mazakk94 <mazakk94@gmail.com>
2016-09-07 19:04:00 +0200

    comming b4 rollback

--- MVVM/WelcomeWindow.xaml.cs
+++ MVVM/WelcomeWindow.xaml.cs
@@ -10,14 +10,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MVVM
 {
-    public partial class MainWindow : Window
+    public partial class WelcomeWindow : Window
     {
-        public MainWindow()
+        public WelcomeWindow()
         {
             InitializeComponent();
         }
